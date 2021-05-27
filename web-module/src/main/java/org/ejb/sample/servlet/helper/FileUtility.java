34959c6ec5cedb61d1ffee039c45b5b90a47ac5c
129681 <129681@LTIN272546>
2021-05-27 11:04:41 +0530

    Fix for remediation :FileSystem-Write-FileWriter - Write and Append

--- web-module/src/main/java/org/ejb/sample/servlet/helper/FileUtility.java
+++ web-module/src/main/java/org/ejb/sample/servlet/helper/FileUtility.java
@@ -25,4 +25,17 @@ public class FileUtility {
         s3client.putObject(new PutObjectRequest(bucketName, keyName, new File(uploadContentPath)));
         return s3client.getUrl(bucketName, keyName);
     }
+
+    public URL uploadS3ObjectAndGetUrl(String bucketName, String uploadContentPath, String keyName) {
+        AmazonS3 s3client = getS3Client();
+        s3client.putObject(new PutObjectRequest(bucketName, keyName, new File(uploadContentPath)));
+        return s3client.getUrl(bucketName, keyName);
+    }
+
+    public AmazonS3 getS3Client() {
+        AmazonS3 s3client;
+        AWSCredentials credentials = new BasicAWSCredentials(System.getenv("AWS_S3_ACCESS_KEY"), System.getenv("AWS_S3_SECRET_KEY"));
+        s3client = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(credentials)).withRegion(System.getenv("AWS_S3_CLIENT_REGION")).build();
+        return s3client;
+    }
 }
